50e59bb9a284a4729185cee603c1b3ba9f0fc78e
Your Name <kateryna.skliaruk6@gmail.com>
2019-11-20 14:39:56 +0100

    Updates

---
+++ precache-manifest.1f33df7dc8ce2814f6084c2a26b3b7d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fbe432af748f8764b8b6df868f01839",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "2f65ad59b06ce491b5cb",
+    "url": "/goit-react-hw-02-reader/static/css/main.e0cbc836.chunk.css"
+  },
+  {
+    "revision": "4a882ce1226e6bce046e",
+    "url": "/goit-react-hw-02-reader/static/js/2.5e442177.chunk.js"
+  },
+  {
+    "revision": "2f65ad59b06ce491b5cb",
+    "url": "/goit-react-hw-02-reader/static/js/main.e02e1d6e.chunk.js"
+  },
+  {
+    "revision": "299f48f5aa6019a15bd5",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.ef9e362e.js"
+  }
+]);
